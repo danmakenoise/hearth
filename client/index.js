9dad0300e90910ea2d1cb2b264e034bd2bb7df86
danmakenoise <danmakenoise@gmail.com>
2018-11-05 23:14:37 -0500

    add react to client bundle

--- client/index.js
+++ client/index.js
@@ -1,2 +1,4 @@
-/* eslint-disable no-console */
-console.log("Welcome to the bundle!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Heart</h1>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: "./client/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, "server/public"),
     filename: "bundle.js"
